3f415dfae228f1df3eeb2db8e8e84b8a7f957108
Jo Aleksander Kvalen <jo.kvalen@onecall.no>
2016-07-21 14:29:37 +0200

    ADD: content from other site

---
+++ nav.php
@@ -0,0 +1,13 @@
+</head>
+<body>
+
+<header class="header">
+	<a href="index.html" class="logo">Villa Ekorren</a>
+	<nav class="nav">
+		<ul class="nav-list">
+			<li><a href="kart/index.html">Kart</a></li>
+			<li><a href="om-ekorren/index.html">Om Ekorren</a></li>
+			<li><a href="kontakt/index.html">Kontakt</a></li>
+		</ul>
+	</nav>
+</header>
