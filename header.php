12469a6bd54129c12a6df3530d112cb01fede067
Jo Aleksander Bakke Kvalen <jo.kvalen@gmail.com>
2019-08-01 14:01:21 +0200

    Cleanup

--- header.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html lang="no">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<meta name="google-site-verification" content="_rV3T2OzLhdNRjFeE03OCx1LEBYnel5oXmdDlXZSfjw"/>
-	<meta name="viewport" content="width=device-width">
-	<link href="css/style.css" media="all" rel="stylesheet" type="text/css"/>
